dd29e6de3b6318d02276a699970a36c875649d2b
Sonu Kumar <ksonuraj1@gmail.com>
2024-07-27 21:16:04 +0530

    added usermodel

---
+++ NodeServer/src/models/user.model.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    emailId: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('User', userSchema);
+export { User };
